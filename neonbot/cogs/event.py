719b385935379cd215433f59db9e142bbdeacdf9
NeonSpectrum <youngskymann@gmail.com>
2019-09-30 18:22:57 +0800

    Fix event spotify same title

--- neonbot/cogs/event.py
+++ neonbot/cogs/event.py
@@ -155,15 +155,17 @@ class Event(commands.Cog):
             last = before.activities and before.activities[-1]
             current = after.activities and after.activities[-1]
 
+            # If not spotify, game name must not be the same
             if not isinstance(current, discord.Spotify) and (
                 getattr(last, "name", None) == getattr(current, "name", None)
             ):
                 return
 
-            embed.description = f":bust_in_silhouette:**{before.name}** is"
-            embed.set_author(
-                name="Activity Presence Update", icon_url=bot.user.avatar_url
-            )
+            # If spotify, song title must not be the same
+            if isinstance(current, discord.Spotify) and (
+                getattr(last, "title", None) == getattr(current, "title", None)
+            ):
+                return
 
             def get_image(
                 activity: Union[discord.Spotify, discord.Game, discord.Activity]
@@ -173,6 +175,11 @@ class Event(commands.Cog):
                 return getattr(activity, "large_image_url", None) or (
                     getattr(activity, "small_image_url", None)
                 )
+
+            embed.description = f":bust_in_silhouette:**{before.name}** is"
+            embed.set_author(
+                name="Activity Presence Update", icon_url=bot.user.avatar_url
+            )
 
             if isinstance(current, discord.Spotify):
                 embed.set_thumbnail(url=get_image(current))
