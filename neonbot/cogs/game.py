682153f999a6fcf6011a9a649e215577d10ed795
NeonSpectrum <youngskymann@gmail.com>
2020-06-12 16:50:21 +0800

    Update game not working

--- neonbot/cogs/game.py
+++ neonbot/cogs/game.py
@@ -12,12 +12,12 @@ from ..helpers.log import Log
 log = cast(Log, logging.getLogger(__name__))
 
 
-def get_channel(channel: discord.TextChannel) -> Dict:
+def get_channel(ctx: commands.Context) -> Dict:
     rooms = bot.game
-    if channel.id not in rooms.keys():
-        rooms[channel.id] = Dict(pokemon=Pokemon(channel), connect4=Connect4(channel))
+    if ctx.channel.id not in rooms.keys():
+        rooms[ctx.channel.id] = Dict(pokemon=Pokemon(ctx), connect4=Connect4(ctx))
 
-    return rooms[channel.id]
+    return rooms[ctx.channel.id]
 
 
 class Game(commands.Cog):
@@ -26,7 +26,7 @@ class Game(commands.Cog):
     async def pokemon(self, ctx: commands.Context, *, command: str) -> None:
         """Starts, stops, or shows the scoreboard of the pokemon game."""
 
-        pokemon = get_channel(ctx.channel).pokemon
+        pokemon = get_channel(ctx).pokemon
 
         if command == "start":
             if pokemon.status == 1:
@@ -53,7 +53,7 @@ class Game(commands.Cog):
     async def connect4(self, ctx: commands.Context) -> None:
         """Starts connect4 game and waits for the players if players are insufficient."""
 
-        connect4 = get_channel(ctx.channel).connect4
+        connect4 = get_channel(ctx).connect4
         if connect4.players == 2:
             await ctx.send(
                 embed=Embed("Connect4 game is already running"), delete_after=5
