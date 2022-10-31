86e42890a82c1016bac6d996c4080b7de1ef05c5
NeonSpectrum <youngskymann@gmail.com>
2022-10-31 12:54:45 +0800

    Add logging to context_menu

--- neonbot/utils/functions.py
+++ neonbot/utils/functions.py
@@ -16,12 +16,22 @@ async def shell_exec(command: str) -> str:
 
 
 def get_command_string(interaction: discord.Interaction):
-    params = ' '.join([
-        f'{key}="{value}"'
-        for key, value in interaction.namespace.__dict__.items()
-    ])
+    params = []
 
-    return f"{interaction.command.name} {params}"
+    # Context menu starts with uppercase
+    if interaction.command.name[0].isupper():
+        try:
+            users = list(interaction.data['resolved']['users'].values())
+            params = [f"{user['username']}#{user['discriminator']}" for user in users]
+        except IndexError:
+            pass
+    else:
+        params = [
+            f'{key}="{value}"'
+            for key, value in interaction.namespace.__dict__.items()
+        ]
+
+    return f"{interaction.command.name} {' '.join(params)}"
 
 
 def format_seconds(secs: Union[int, float]) -> str:
