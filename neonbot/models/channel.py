a8dbc696f4b5b86f7ea5252a9f08edf01ab1d7a0
NeonSpectrum <youngskymann@gmail.com>
2023-11-14 18:10:22 +0800

    Fix migration error

--- neonbot/models/channel.py
+++ neonbot/models/channel.py
@@ -7,6 +7,5 @@ class Channel(BaseModel):
     voice_log: Optional[int] = None
     status_log: Optional[int] = None
     activity_log: Optional[int] = None
-    presence_log: Optional[int] = None
     msgdelete_log: Optional[int] = None
     chatgpt: Optional[int] = None
