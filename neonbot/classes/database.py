13a43094ed09c17de2e470b90a208d412f7689d1
NeonSpectrum <youngskymann@gmail.com>
2023-11-15 17:30:10 +0800

    Fix error on migration

--- neonbot/classes/database.py
+++ neonbot/classes/database.py
@@ -54,7 +54,7 @@ class Database:
         await Guild.create_instance(guild.id)
 
     async def start_migration(self, guild_id: int):
-        guild = (await self.db.servers.find_one({}, {'_id': guild_id}))
+        guild = await self.db.servers.find_one({'_id': guild_id})
         print(guild)
 
         if 'channel_log' in guild:
