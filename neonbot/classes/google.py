fbf05e838745ad806a1e2b0e04d5622dcf4cfd09
NeonSpectrum <youngskymann@gmail.com>
2025-06-13 14:52:24 +0800

    Code refactor

--- neonbot/classes/google.py
+++ neonbot/classes/google.py
@@ -11,27 +11,21 @@ _credentials_lock = asyncio.Lock()
 async def get_google_access_token():
     global _cached_credentials
 
+    def get_credentials(creds=None):
+        if creds is None:
+            creds, _ = google.auth.default()
+        req = google.auth.transport.requests.Request()
+        creds.refresh(req)
+        return creds
+
     async with _credentials_lock:
+        loop = asyncio.get_running_loop()
+
         if _cached_credentials is None:
-
-            def _sync_get_initial_credentials():
-                creds, _ = google.auth.default()
-                req = google.auth.transport.requests.Request()
-                creds.refresh(req)
-                return creds
-
-            loop = asyncio.get_running_loop()
             with concurrent.futures.ThreadPoolExecutor() as pool:
-                _cached_credentials = await loop.run_in_executor(pool, _sync_get_initial_credentials)
-        else:
-
-            def _sync_refresh_credentials(creds_obj):
-                req = google.auth.transport.requests.Request()
-                creds_obj.refresh(req)
-                return creds_obj
-
-            loop = asyncio.get_running_loop()
+                _cached_credentials = await loop.run_in_executor(pool, get_credentials)
+        elif _cached_credentials.expired:
             with concurrent.futures.ThreadPoolExecutor() as pool:
-                _cached_credentials = await loop.run_in_executor(pool, _sync_refresh_credentials, _cached_credentials)
+                _cached_credentials = await loop.run_in_executor(pool, get_credentials, _cached_credentials)
 
         return _cached_credentials.token
