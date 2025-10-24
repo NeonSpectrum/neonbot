861118ba0640d7444bee0061d5aeb8f0cf4e48f0
NeonSpectrum <youngskymann@gmail.com>
2025-10-25 01:39:22 +0800

    Adjust world boss

--- neonbot/classes/flyff.py
+++ neonbot/classes/flyff.py
@@ -68,7 +68,7 @@ class Flyff:
                 spawn_time = self.calculate_next_spawn(
                     timer.initial_interval,
                     timer.interval,
-                    lambda count: (name == 'Karvan' and count % 2 == 0) or (name == 'Clockworks' and count % 2 == 1),
+                    lambda count: (name == 'The Void' and count % 2 == 0) or (name == 'Karvan' and count % 2 == 1),
                 )
 
                 timers.append(f'- {name}: <t:{spawn_time}:T> <t:{spawn_time}:R>')
