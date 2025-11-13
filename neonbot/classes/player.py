bd7c62fb4876dce82a338f24a21a7e3bb0ce1fe8
NeonSpectrum <youngskymann@gmail.com>
2025-11-13 14:35:39 +0800

    Test

--- neonbot/classes/player.py
+++ neonbot/classes/player.py
@@ -322,8 +322,8 @@ class Player(DefaultPlayer):
 
     async def clear_messages(self):
         if self.messages['finished']:
-            print('sending finished')
-            await bot.edit_message(self.messages['finished'], embed=self.get_simplified_finished_message(self.last_track), view=MISSING)
+            await bot.delete_message(self.messages['finished'])
+            await self.channel.send(embed=self.get_simplified_finished_message(), silent=True)
 
         await bot.delete_message(self.messages['playing'])
         self.messages['playing'] = None
