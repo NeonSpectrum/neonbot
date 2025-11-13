ba1d7ca917296f0d384d54ae7b507b1c4cc7e90a
Manny Young <manny@treasury.jp>
2025-11-13 16:15:52 +0800

    Try to remove warning on scheduler

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ def main() -> None:
     # Clear debug.log on startup
     open('./debug.log', 'w').close()
 
-    scheduler_logger = logging.getLogger('apscheduler.executors')
-    scheduler_logger.setLevel(logging.ERROR)
+    logging.getLogger('apscheduler.scheduler').setLevel(logging.ERROR)
 
     with ThreadPoolExecutor() as executor:
         bot.run(log_level=logging.getLevelName(env.str('LOG_LEVEL', default='ERROR')), executor=executor)
