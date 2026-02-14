3ceccf2be7b314edf779737032b8cff2b768674c
NeonSpectrum <youngskymann@gmail.com>
2026-02-14 18:38:21 +0800

    Test

--- neonbot/cogs/event.py
+++ neonbot/cogs/event.py
@@ -39,7 +39,7 @@ class Event(commands.Cog):
         log.info('Ready!\n')
         bot.set_ready()
         bot.start_listeners()
-        print(YTMusic.get_account_info())
+        print(await YTMusic.get_account_info())
 
     @staticmethod
     @listener(NodeConnectedEvent)
