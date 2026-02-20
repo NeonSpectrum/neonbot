e8c13b22fe37d8214f5e7b66ad402e4c3d7add41
NeonSpectrum <youngskymann@gmail.com>
2026-02-20 22:27:56 +0800

    Test

--- neonbot/classes/player.py
+++ neonbot/classes/player.py
@@ -283,6 +283,8 @@ class Player(DefaultPlayer):
 
             try:
                 track = self.playlist[self.current_queue]
+                print(self.current_queue)
+                print(track)
             except IndexError:
                 log.error(f'Playlist length is {len(self.playlist)}. Current queue is {self.current_queue}')
                 return
