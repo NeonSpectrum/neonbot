35f1571102a021bc93a6abbf9944b48b688b2d6e
NeonSpectrum <youngskymann@gmail.com>
2026-02-21 00:53:20 +0800

    Fix bugs

--- neonbot/cogs/music.py
+++ neonbot/cogs/music.py
@@ -76,6 +76,7 @@ class Music(commands.Cog):
         await player.connect()
 
         if not player.is_playing:
+            await player.queue_next_song()
             await player.play()
 
     @commands.hybrid_command(name='playrandom', aliases=['pr'], )
