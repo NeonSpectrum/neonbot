6f121e83d3dcadc140b11633c0160163e0c1ed8d
NeonSpectrum <youngskymann@gmail.com>
2026-02-20 23:50:16 +0800

    Test

--- neonbot/cogs/updater.py
+++ neonbot/cogs/updater.py
@@ -106,7 +106,7 @@ class UpdaterCog(commands.Cog):
 
         # noinspection PyShadowingNames
         async def replace(guild_id, new_player):
-            player = bot.lavalink.player_manager.create(guild_id)
+            player: Player = bot.lavalink.player_manager.create(guild_id)
             position = new_player.pop('position')
             player.__dict__.update(new_player)
             await player.execute_fn_without_event(
