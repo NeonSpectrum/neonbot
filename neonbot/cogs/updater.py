ff9b38642c221f4271bbdb7cd234bd7a388d72ed
NeonSpectrum <youngskymann@gmail.com>
2026-02-20 23:18:05 +0800

    Test

--- neonbot/cogs/updater.py
+++ neonbot/cogs/updater.py
@@ -129,7 +129,7 @@ class UpdaterCog(commands.Cog):
                             player = bot.lavalink.player_manager.create(guild_id)
                             position = new_player.pop('position')
                             player.__dict__.update(new_player)
-                            await player.execute_fn_without_event(
+                            await player.execute_fn_without_message(
                                 player.play(player.current, start_time=position)
                             )
 
