047873587d0bf651863fd4d24a3aa2354c6f74c3
NeonSpectrum <youngskymann@gmail.com>
2026-02-21 19:32:12 +0800

    Add move channel if not the same on connect

--- neonbot/classes/player.py
+++ neonbot/classes/player.py
@@ -105,13 +105,17 @@ class Player(DefaultPlayer):
         log.cmd(self.ctx, msg)
         await self.send_message(embed=Embed(msg))
 
-    async def connect(self, voice_channel=None):
+    async def connect(self, voice_channel: discord.VoiceChannel = None):
         if self.ctx.guild.voice_client:
+            if self.ctx.guild.voice_client.channel != voice_channel:
+                self.vc = voice_channel
+                await self.ctx.guild.me.move_to(voice_channel)
+                log.cmd(self.ctx, t('music.player_connected', channel=self.vc))
+
             return
 
         self.vc = voice_channel or self.ctx.author.voice.channel
         await self.vc.connect(timeout=3, reconnect=True, self_deaf=True, cls=LavalinkVoiceClient)
-
         log.cmd(self.ctx, t('music.player_connected', channel=self.vc))
 
     async def disconnect(self, force=True, timeout=None) -> None:
