e98529b1326265c26d8eea27c77a35999f0ca93a
NeonSpectrum <youngskymann@gmail.com>
2026-02-21 15:19:17 +0800

    Test

--- neonbot/cogs/event.py
+++ neonbot/cogs/event.py
@@ -96,9 +96,13 @@ class Event(commands.Cog):
                     for cmd in cmds:
                         command, arguments = cmd
 
-                        ctx.command = command
-                        ctx.args = list(arguments.values())
-                        await bot.invoke(ctx)
+                        message = discord.Message(
+                            content=f"{bot.default_prefix}{command.name} {' '.join(arguments.values())}",
+                            channel=ctx.channel
+                        )
+
+                        new_ctx = await bot.get_context(message)
+                        await bot.invoke(new_ctx)
             except Exception as error:
                 await ctx.reply(embed=Embed('Something went wrong.'))
                 log.debug(error, exc_info=True)
