4a71c6fc9ddb316044b51f245095aff1521160f3
NeonSpectrum <youngskymann@gmail.com>
2026-02-23 17:36:24 +0800

    Test

--- neonbot/cogs/music.py
+++ neonbot/cogs/music.py
@@ -76,7 +76,7 @@ class Music(commands.Cog):
         await player.connect()
 
         if not player.is_playing:
-            await player.play()
+            await player.play_next()
 
     @commands.hybrid_command(name='playrandom', aliases=['pr'], )
     @commands.check(has_permission)
@@ -94,7 +94,7 @@ class Music(commands.Cog):
         await player.connect()
 
         if not player.is_playing:
-            await player.play()
+            await player.play_next()
 
     @commands.hybrid_command(name='nowplaying', aliases=['np'])
     @commands.check(has_permission)
