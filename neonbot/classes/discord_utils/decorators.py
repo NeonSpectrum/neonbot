e0359e8d14bedc3f91a0015c6bb57e60e84e07a8
NeonSpectrum <youngskymann@gmail.com>
2026-04-10 21:14:57 +0800

    Add more condition on has_player

--- neonbot/classes/discord_utils/decorators.py
+++ neonbot/classes/discord_utils/decorators.py
@@ -56,7 +56,7 @@ def has_player(app: bool = False) -> Check[Context['NeonBot']]:
 
         player: Player = ctx.bot.get_player_instance(ctx.guild.id)
 
-        if not player or not player.ctx:
+        if not player or not player.ctx or len(player.playlist) == 0:
             await ctx.reply(embed=Embed('No active player.'), ephemeral=True)
             return False
         return True
