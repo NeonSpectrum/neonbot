dc5cd253f70568c6a037e66d71315a244f10acae
NeonSpectrum <youngskymann@gmail.com>
2026-04-10 21:01:44 +0800

    Remove message after setting ctx

--- neonbot/classes/player/player.py
+++ neonbot/classes/player/player.py
@@ -103,6 +103,7 @@ class Player(DefaultPlayer):
             message = await voice_channel.send('Executing autoplay...')
             ctx = await self.bot.get_context(message)
             self.set_ctx(ctx, save_last_channel_id=False)
+            await message.delete()
             return
 
         if not self.ctx:
